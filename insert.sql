c4b953c7b39fee92de52f62855185255f57a79f7
Halldór Þorri Ásgeirsson <halldoras206@nemi.tskoli.is>
2016-12-01 16:40:57 +0000

    Images og insert

--- insert.sql
+++ insert.sql
@@ -9,4 +9,10 @@ insert into item (itemName, itemDescription, itemImgPath, itemPrice, category_ID
 values('Screwdriver 1','Turns the screws','images/1.jpg',500,1),
 ('Screwdriver 2', 'Turns the screws, more efficiently','images/2.jpg',550,1),
 ('Drill 1','Turns the screws for you','images/3.jpg',1000,2),
-('Drill 2','Turns the screws for you and makes you coffee','images/4.png',1500,2)
+('Drill 2','Turns the screws for you and makes you coffee','images/4.png',1500,2),
+('Hammer 1','Bangs you nails in with power','images/9.jpg',150,4),
+('Hammer 2','Bangs you nails in with power and style','images/10.jpg',200,4),
+('Nail','Sticks things together','images/5.jpg',10,3),
+('Screw','Sticks things together better than nails','images/6.png',15,3),
+('Miter saw','Handsawing is in the past','images/7.jpg',2000,5),
+('Bench grinder','Sharp tools for sharp people','images/8.jpg',1250,5)
